59a1933eed883044c5bed2bf2fd8244a2a666bd7
FaaizHaikal <faaizhaikal@gmail.com>
2025-03-16 13:55:32 +0700

    feat: main process

---
+++ src/main.cpp
@@ -0,0 +1,223 @@
+#include "action.h"
+#include "cmd_process.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <term.h>
+#include <ncurses.h>
+#include <libgen.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void change_current_dir()
+{
+    char exepath[1024] = {0};
+    if(readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
+        chdir(dirname(exepath));
+}
+
+void sighandler(int sig)
+{
+    struct termios term;
+    tcgetattr( STDIN_FILENO, &term );
+    term.c_lflag |= ICANON | ECHO;
+    tcsetattr( STDIN_FILENO, TCSANOW, &term );
+
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGABRT, &sighandler);
+    signal(SIGTERM, &sighandler);
+    signal(SIGQUIT, &sighandler);
+    signal(SIGINT, &sighandler);
+
+    int ch;
+    char filename[128];
+    if (argc > 1) {
+        snprintf(filename, sizeof(filename), "%s", argv[1]);
+    } else {
+        printf("Missing filename!\n");
+        printf("Usage: %s <filename>\n", argv[0]);
+
+        return 0;
+    }
+
+    change_current_dir();
+
+    /////////////// Load/Create Action File //////////////////
+    if(!Action::GetInstance()->LoadFile(filename))
+    {
+        printf("Can not open action file\n");
+        return 0;
+    }
+
+    /////////////////////////////////////////////////////////////////////
+
+    DrawIntro();
+
+    while(1)
+    {
+        ch = _getch();
+
+        if(ch == 0x1b)
+        {
+            ch = _getch();
+            if(ch == 0x5b)
+            {
+                ch = _getch();
+                if(ch == 0x41)      // Up arrow key
+                    MoveUpCursor();
+                else if(ch == 0x42) // Down arrow key
+                    MoveDownCursor();
+                else if(ch == 0x44) // Left arrow key
+                    MoveLeftCursor();
+                else if(ch == 0x43) // Right arrow key
+                    MoveRightCursor();
+            }
+        }
+        else if( ch >= 'A' && ch <= 'z' )
+        {
+            char input[128] = {0,};
+            char *token;
+            int input_len;
+            char cmd[80];
+            int num_param;
+            int iparam[30];
+
+            int idx = 0;
+
+            BeginCommandMode();
+
+            printf("%c", ch);
+            input[idx++] = (char)ch;
+
+            while(1)
+            {
+                ch = _getch();
+                if( ch == 0x0A )
+                    break;
+                else if( ch == 0x7F )
+                {
+                    if(idx > 0)
+                    {
+                        ch = 0x08;
+                        printf("%c", ch);
+                        ch = ' ';
+                        printf("%c", ch);
+                        ch = 0x08;
+                        printf("%c", ch);
+                        input[--idx] = 0;
+                    }
+                }
+                else if( ( ch >= 'A' && ch <= 'z' ) || ch == ' ' || ( ch >= '0' && ch <= '9'))
+                {
+                    if(idx < 127)
+                    {
+                        printf("%c", ch);
+                        input[idx++] = (char)ch;
+                    }
+                }
+            }
+
+            fflush(stdin);
+            input_len = strlen(input);
+            if(input_len > 0)
+            {
+                token = strtok( input, " " );
+                if(token != 0)
+                {
+                    strcpy( cmd, token );
+                    token = strtok( 0, " " );
+                    num_param = 0;
+                    while(token != 0)
+                    {
+                        iparam[num_param++] = atoi(token);
+                        token = strtok( 0, " " );
+                    }
+
+                    if(strcmp(cmd, "exit") == 0)
+                    {
+                        if(!AskSave())
+                            break;
+                    }
+                    else if (strcmp(cmd, "json") == 0)
+                        WriteJson();
+                    else if(strcmp(cmd, "re") == 0)
+                        DrawPage();
+                    else if(strcmp(cmd, "help") == 0)
+                        HelpCmd();
+                    else if(strcmp(cmd, "n") == 0)
+                        NextCmd();
+                    else if(strcmp(cmd, "b") == 0)
+                        PrevCmd();
+                    else if(strcmp(cmd, "time") == 0)
+                        TimeCmd();
+                    else if(strcmp(cmd, "speed") == 0)
+                        SpeedCmd();
+                    else if(strcmp(cmd, "page") == 0)
+                    {
+                        if(num_param > 0)
+                            PageCmd(iparam[0]);
+                        else
+                            PrintCmd("Need parameter");
+                    }
+                    else if(strcmp(cmd, "list") == 0)
+                        ListCmd();
+                    else if(strcmp(cmd, "w") == 0)
+                    {
+                        if(num_param > 0)
+                            WriteStepCmd(iparam[0]);
+                        else
+                            PrintCmd("Need parameter");
+                    }
+                    else if(strcmp(cmd, "d") == 0)
+                    {
+                        if(num_param > 0)
+                            DeleteStepCmd(iparam[0]);
+                        else
+                            PrintCmd("Need parameter");
+                    }
+                    else if(strcmp(cmd, "i") == 0)
+                    {
+                        if(num_param == 0)
+                            InsertStepCmd(0);
+                        else
+                            InsertStepCmd(iparam[0]);
+                    }
+                    else if(strcmp(cmd, "m") == 0)
+                    {
+                        if(num_param > 1)
+                            MoveStepCmd(iparam[0], iparam[1]);
+                        else
+                            PrintCmd("Need parameter");
+                    }
+                    else if(strcmp(cmd, "copy") == 0)
+                    {
+                        if(num_param > 0)
+                            CopyCmd(iparam[0]);
+                        else
+                            PrintCmd("Need parameter");
+                    }
+                    else if(strcmp(cmd, "new") == 0)
+                        NewCmd();
+                    else if(strcmp(cmd, "save") == 0)
+                        SaveCmd();
+                    else if(strcmp(cmd, "name") == 0)
+                        NameCmd();
+                    else
+                        PrintCmd("Bad command! please input 'help'");
+                }
+            }
+
+            EndCommandMode();
+        }
+    }
+
+    DrawEnding();
+
+    return 0;
+}
